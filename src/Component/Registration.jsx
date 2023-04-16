fb43e726501b40d8a60144c0b927c25483d78fd6
nuci <kirianuci@gmail.com>
2023-04-16 18:37:48 +0400

    Add registration page design with user input fields

--- src/Component/Registration.jsx
+++ src/Component/Registration.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Style/registration.css";
 
 const Registration = () => {
-  return (
-    <div>Registration</div>
-  )
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Registration
+  const handleFirstNameChange = (e) => setFirstName(e.target.value);
+  const handleLastNameChange = (e) => setLastName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="registration_container">
+      <form onSubmit={handleSubmit} className="registration_form">
+        <label className="registration_label" htmlFor="firstName">First Name:</label>
+        <input
+          className="registration_input"
+          type="text"
+          name="firstName"
+          placeholder="Type your first name..."
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+        
+        <label className="registration_label" htmlFor="lastName">Last Name:</label>
+        <input
+          className="registration_input"
+          type="text"
+          name="lastName"
+          placeholder="Type your last name..."
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+        
+        <label className="registration_label" htmlFor="email">Email:</label>
+        <input
+          className="registration_input"
+          type="email"
+          name="email"
+          placeholder="Type your email..."
+          value={email}
+          onChange={handleEmailChange}
+        />
+        
+        <label className="registration_label" htmlFor="password">Password:</label>
+        <input
+          className="registration_input"
+          type="password"
+          name="password"
+          placeholder="Type your password..."
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        
+        <button className="registration_btn" type="submit">Register</button>
+        
+        <Link to="/">
+          <button className="back_btn" type="submit">Back</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
