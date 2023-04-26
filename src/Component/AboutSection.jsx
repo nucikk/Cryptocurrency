19e51b861991f68b8b03f79a51e601bb7c1d5e76
nuci <kirianuci@gmail.com>
2023-04-26 20:22:23 +0400

    Add search buttons and responsive design

--- src/Component/AboutSection.jsx
+++ src/Component/AboutSection.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style/about.css";
+import { Link } from "react-router-dom";
 
 function AboutSection() {
+  const buttons = [
+    { name: "View all"},
+    { name: "Crypto"},
+    { name: "Blockchain"},
+    { name: "Announcements"},
+    { name: "People"},
+    { name: "Engineering"},
+    { name: "NTF"},
+  ];
+
   return (
-    <div className="blog_section">
-      <div className="news_section">
-        <h3 className="blog_heading">Blog</h3>
-        <h1 className="news_heading">News and insights</h1>
-        <div className="news_description">
-          <p>Learn about cryptocurrency, NFTs, and blockchain, </p>
-          <p>discover our latest product updates, partnership</p>
-          <p>announcements, user stories, and more.</p>
+    <>
+      <div className="blog_section">
+        <div className="news_section">
+          <h3 className="blog_heading">Blog</h3>
+          <h1 className="news_heading">News and insights</h1>
+          <div className="news_description">
+            <p>Learn about cryptocurrency, NFTs, and blockchain, </p>
+            <p>discover our latest product updates, partnership</p>
+            <p>announcements, user stories, and more.</p>
+            <div className="news_box_btn">
+              {buttons.map((button, index) => (
+                <button key={index} className="btn_insight"> {button.name} </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
